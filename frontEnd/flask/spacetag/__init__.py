777663d5574d07919a91418be797be154567ab8d
northdecoder <northdecoder@users.noreply.github.com>
2021-05-25 23:30:24 +0000

    fix(init.py): add template,css,img directory
    - add
      - in __init__.py
        - render_template and template_folder
        - app.run
      - directory static/css/bootstrp.min.css
    
    - change /templates/index.html
      - http to https

--- frontEnd/flask/spacetag/__init__.py
+++ frontEnd/flask/spacetag/__init__.py
@@ -1,9 +1,20 @@
 #!./venv/bin/python
 
-from flask import Flask
+from flask import Flask, render_template
+
+
 
 # creates the app instance using the name of the module
-app = Flask(__name__)
+app = Flask( __name__, template_folder='templates' )
 
-print(__name__, " app created.") # to remove later
 
+print(__name__, " app created.") # DEBUG ONLY
+
+
+# https://replit.com/talk/learn/Flask-Tutorial/36529
+@app.route('/') # Route the Function
+def main(): # Run the function
+    x = 'String' # Set x to 'String'
+    return render_template( 'index.html', x=x ) # Render the template with a variable
+
+app.run(host='0.0.0.0', port=5000, debug=True) # Run the Application (in debug mode)
