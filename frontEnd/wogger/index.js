2b519388429f822bc050f1eba3570882d60dbcf2
northdecoder <northdecoder@users.noreply.github.com>
2022-02-03 22:27:39 -0800

    feat(wogger/index.js): add cli arg logger formats

--- frontEnd/wogger/index.js
+++ frontEnd/wogger/index.js
@@ -6,9 +6,43 @@
  * */
 
 const winston = require('winston');
+const commandLineArgs = process.argv.slice(2);
+
+var requestedLogFormat = ''
+
+commandLineArgs.forEach( argument => instruction(argument) )
+
+function instruction(cla) {
+
+  lvrvArray = cla.split("=") // right and left val
+
+  switch(lvrvArray[0]) {
+    case '--logformat':
+      if ( lvrvArray[1]=== 'simple' ){
+         requestedLogFormat  = winston.format.simple()
+      }
+
+      if ( lvrvArray[1]=== 'json' ){
+         requestedLogFormat  = winston.format.json()
+      }
+      break;
+  }//end switch
+}
+/**
+ * expecting  commandLineArgs to receive an array as follows
+ *  [ '--loglevel=info', '--debuglevel=1', '--runlevel=production' ]
+ *  OR
+ *  [ '--help']
+ *  OR
+ *  ['']
+ *
+ *  Usage:
+ *
+ *  node server.js --loglevel=info --logformat=json --debuglevel=1 --runlevel=production
+ * */
 
 const wogger = winston.createLogger({
-  format: winston.format.simple(),
+  format: requestedLogFormat,
   transports: [new winston.transports.Console()]
 });
 
