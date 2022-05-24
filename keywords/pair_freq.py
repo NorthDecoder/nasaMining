0ecaeea79e49852594cf656271e556c6fd3ab288
northdecoder <northdecoder@users.noreply.github.com>
2022-05-24 14:55:52 -0700

    refactor(pair_freq.py): add helper class part two
    - change style of comments
    - still needs some care to fix a TypeError

--- keywords/pair_freq.py
+++ keywords/pair_freq.py
@@ -8,44 +8,117 @@ from math import log
 
 class Helper:
     def __init__(self):
-        ''' Help for nasa_kw_pair_freq.py '''
+        ''' Help for pair_freq.py '''
 
-    def nasa_kw_pair_freq_augmented_help():
+    def pair_freq_augmented_help():
         '''
+        # Keyword pair frequency count
+
+        > Original contributor: flava flav
+
+        ## Overview
+
         Generate a list of frequency counts for keyword pairs,
         sorted in reverse order by total.
+
+        The script pair_freq.py is user configurable by
+        command line arguments which determine
+        where to acquire the input data file,
+        the field in the input file to acquire the keywords,
+        and the name and path of the output file.
+
+        The script is generic and can be used on
+        any dataset that is in the expected input
+        format.  The default is to look for the
+        nasaMining inputs, however the default
+        inputs can be changed to the desired input.
+
+        ## Usage
+
+        python3 keywords/pair_freq.py \\
+                --input ../data/nasa_keywords.json \\
+                --field ngram_keywords \\
+                --output ../data/nasa_np_strengths.json
+
+        ## Inputs
+
+        The format of the json input file is expected
+        to be like:
+                    {"dataset":[{},{},{}]}
+
+        Each object in the json array must have a
+        name which can be identified by the argument
+        something like:
+
+        --field ngram_keywords
+
+        [ {"ngram_keywords":["real-time","sea surface","temperature"]},
+          {"ngram_keywords":["foundation","sea surface","temperature"]},
+          {"ngram_keywords":["global","distribution","node"]}
+        ]
+
+
+        ## Output
+
+        The format of the json output file will be like
+        {[{},{},{}]}
+
+        Each object of the json array will have data something like
+        {"count": 340.0,
+         "a": 7492,
+         "b": 7492,
+         "keyword": ["soil moisture accuracy", "soil moisture algorithm performance"],
+         "pmi_doc": -1.6840664693580758,
+         "pmi_kw": -1.7938470459262326
+        }
+
+
+        ## Reference
+
+        - Acronyms
+          - np:   noun phrase
+          - pmi:  pointwise mutual information
+          - dpmi: dataset pointwise mutual information
+          - kpmi: keyword pointwise mutual information
+
+        - 6.6 Pointwise Mutual Information, pdf pg 109
+          - Speech and Language Processing, 3rd Edition draft,
+            Daniel Jurafsky, Stanford University
+
+        - [Noun Phrase](https://en.wikipedia.org/wiki/Noun_phrase)
+           a phrase that has a noun or pronoun as its head
+
+
         '''
 #
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Keyword pair frequency extraction')
 
-    parser.add_argument('-i', "--input", type=str, default='./data/nasa_keywords.json', \
+    parser.add_argument('-i', "--input", type=str, default='../data/nasa_keywords.json', \
                          help='path to a data.json format file containing keyword ngrams')
 
-    parser.add_argument('-o', "--output", type=str, default='nasa_np_strengths.json', \
+    parser.add_argument('-f', "--field", type=str, default='ngram_keywords', \
+                         help='field in the dataset in which to find the keywords')
+
+    parser.add_argument('-o', "--output", type=str, default='../data/nasa_np_strengths.json', \
                          help='path to output the data with extracted keywords')
 
-    parser.add_argument('-f', "--field", type=str, default='description_textrank_kw', \
-                         help='field in each dataset to store the keywords')
-
-    parser.add_argument("--augmented_help", action='store_true', default="", help="Verbose help")
+    parser.add_argument("--augmented_help", action='store_true', default="", \
+                         help="Verbose help")
 
 
     args = parser.parse_args()
 
     if args.augmented_help:
-        help( Helper.nasa_kw_pair_freq_augmented_help )
+        help( Helper.pair_freq_augmented_help )
         exit()
 
     path_to_input_json  = args.input
     path_to_output_json = args.output
     kw_field            = args.field
 
-#     kw_field = 'keyword'
-#     kw_field = 'description_textrank_kw'
-#     kw_field = 'description_bigram_kw'
-#     kw_field = 'gensim_ngram_np'
+    #
 
     data = json.load( open( path_to_input_json )  )
     dataset = data['dataset']
@@ -54,11 +127,15 @@ if __name__ == '__main__':
 
     pairs = {}
     single_words = {}
+
+    #
+
     for i, ds in enumerate(dataset):
         for pair in combinations(ds[kw_field], 2):
             sl_pair = sorted([x.lower() for x in pair], key=unicode.lower)
 
-            if sl_pair[0] != sl_pair[1]: # there are projects with duplicate keywords in the metadata
+            if sl_pair[0] != sl_pair[1]:
+                # if there are projects with duplicate keywords in the metadata
 
                 key = str(sl_pair)
                 keyholder[key] = sl_pair
